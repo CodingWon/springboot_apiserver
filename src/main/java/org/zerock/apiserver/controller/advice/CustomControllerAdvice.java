0b9703ea821f121437e7444f4d56b9ca2a107102
CodingWon <kingcoding2298@gmail.com>
2024-07-14 17:25:03 +0900

    RestControllerAdvice - NosuchElementException 추가 : 페이지를 못찾았을 때

---
+++ src/main/java/org/zerock/apiserver/controller/advice/CustomControllerAdvice.java
@@ -0,0 +1,19 @@
+package org.zerock.apiserver.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+//예외처리를
+@RestControllerAdvice
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> notExist(NoSuchElementException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("msg",e.getMessage()));
+    }
+}
