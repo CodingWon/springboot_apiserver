e2a498becff496c7db2e03c7b40de6ad7fa295b2
CodingWon <kingcoding2298@gmail.com>
2024-07-14 21:30:18 +0900

    CORS 설정 추가

--- src/main/java/org/zerock/apiserver/config/CustomServletConfig.java
+++ src/main/java/org/zerock/apiserver/config/CustomServletConfig.java
@@ -3,6 +3,7 @@ package org.zerock.apiserver.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.apiserver.controller.formatter.LocalDateFormatter;
 
@@ -19,5 +20,19 @@ public class CustomServletConfig implements WebMvcConfigurer {
         log.info("addFormatters");
 
         registry.addFormatter(new LocalDateFormatter());
+    }
+    /*
+    * 외부에서 apiServer 로 요청을 보낼때 CORS 동일출처 원칙을 준수하기 위해 추가한다.
+    * 
+    * pre-flight
+    *   json 요청시 미리 확인 해보는 작업
+    * */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .maxAge(500)
+                .allowedMethods("GET","POST","PUT","DELETE","HEAD","OPTIONS")
+                .allowedOrigins("*");
+        
     }
 }
